55488c697047ac64f7ad4ada75c1c72d1dea7430
nshimiyimanaamani2@gmail.com <nshimiyimanaamani2@gmail.com>
2022-05-22 15:25:17 +0200

    test

---
+++ footer.php
@@ -0,0 +1,21 @@
+<footer class="section page-footer page-footer-minimal novi-background bg-cover text-center bg-gray-darker">
+        <div class="container container-wide">
+          <div class="row row-fix justify-content-sm-center align-items-md-center row-30">
+            <div class="col-md-10 col-lg-7 col-xl-4 text-xl-left"><a href="index.html"><img class="inverse-logo" src="" alt="" width="208" height="46"/></a></div>
+            <div class="col-md-10 col-lg-7 col-xl-4">
+              <p class="right">&#169;&nbsp;<span class="copyright-year"></span> All Rights Reserved. Design&nbsp;by&nbsp;<a href="Terms.html">Terms and condition</a>
+                <br><a href="Privacy.html">Privacy Policy</a></p>
+              
+            </div>
+            <div class="col-md-10 col-lg-7 col-xl-4 text-xl-right">
+              <ul class="group-xs group-middle"> 
+                <li><a class="icon novi-icon icon-md-middle icon-circle icon-secondary-5-filled mdi mdi-facebook" href="https://ms-my.facebook.com/Travel-Team-Rwanda-Ltd-106707848681450/"></a></li>
+                <li><a class="icon novi-icon icon-md-middle icon-circle icon-secondary-5-filled mdi mdi-twitter" href="https://twitter.com/TRAVELTEAM_RW"></a></li>
+                <li><a class="icon novi-icon icon-md-middle icon-circle icon-secondary-5-filled mdi mdi-instagram" href="https://www.instagram.com/travelteamrwanda/"></a></li>
+                <li><a class="icon novi-icon icon-md-middle icon-circle icon-secondary-5-filled mdi mdi-google" href="https://www.google.com/search?q=travel+team+rwanda&sxsrf=ALiCzsYJyroRVoudD_sM9fpsDxK0L9kKNQ%3A1652547228393&ei=nN5_Yqy-F6-YlwTymKDIDw&oq=tr&gs_lcp=Cgdnd3Mtd2l6EAMYADIECCMQJzIECCMQJzIECCMQJzIECAAQQzIFCAAQkQIyBAgAEEMyBQgAEJECMgoIABCxAxCDARBDMgoILhDHARCvARBDMgQIABBDOgcIABBHELADOgcIIxDqAhAnOhAILhCxAxCDARDHARDRAxBDOgsIABCABBCxAxCDAToFCAAQgARKBAhBGABKBAhGGABQhBBYhRxg_yNoA3ABeAGAAeIOiAHAGJIBCzMtMS42LTEuMC4xmAEAoAEBsAEKyAEIwAEB&sclient=gws-wiz"></a></li>
+                <li><a class="icon novi-icon icon-md-middle icon-circle icon-secondary-5-filled mdi mdi-linkedin" href="https://www.linkedin.com/in/travel-team-964753238/"></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
